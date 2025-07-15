380484b278fad57c809c9fd60dbfd622873253a0
Florin Tobler <florin.tobler@hotmail.com>
2025-07-16 01:18:09 +0200

    mockup io calls implementation

--- util/syscalls.c
+++ util/syscalls.c
@@ -3,8 +3,29 @@
 // Define the global errno variable.
 int errno;
 
-// newlib-nano needs this symbol as well.
-// It is usually a pointer to the errno variable.
+// newlib-nano needs these symbols.
+
+
 int * __errno(void) {
   return &errno;
+}
+
+int _close(int file)
+{
+	return -1;
+}
+
+int _read(int file, char *ptr, int len)
+{
+	return 0;
+}
+
+int _write(int file, char *ptr, int len)
+{
+	return len;
+}
+
+int _lseek(int file, int ptr, int dir)
+{
+	return 0;
 }
